34b8b0945f7ab4c882b27c0668a865440a4bf420
Barinov Maksim <craftyplonkton@gmail.com>
2022-09-10 06:40:52 +0300

    ready for deploy v 2.1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -101,7 +101,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',
                                 'rest_framework.filters.SearchFilter'),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
     'SEARCH_PARAM': 'name'
 }
